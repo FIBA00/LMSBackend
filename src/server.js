6d683811698bf5036753a5c8004278f747aaa857
FIBA00 <Fraolbulti0@gmail.com>
2026-08-18 01:41:37 +0300

    - refactor: added cookie parsing and access token generation into controllers. - new: clear cookie and create access token in the auth middle wares.

--- src/server.js
+++ src/server.js
@@ -1,5 +1,7 @@
 import express from "express";
 import morgan from "morgan";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 import process from "node:process";
 
 // internal imports
@@ -10,6 +12,13 @@ import registerAdminRoutes from "./admin/admin.js";
 const app = express();
 const PORT = process.env.PORT;
 
+app.user(
+	cors({
+		origin: process.env.CLIENT_URL, // e.g. "http://localhost:5173" — must be explicit, not "*"
+		credentials: true,
+	}),
+);
+app.use(cookieParser());
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(morgan("common"));
@@ -21,9 +30,9 @@ app.get("/health", (req, res) =>
 	}),
 );
 
+registerRoutes(app);
+registerAdminRoutes(app);
+
 app.listen(PORT, () => {
 	console.log(`Server is running on port ${PORT}`);
 });
-
-registerRoutes(app);
-registerAdminRoutes(app);
